f30212bd48d7317f5274908d7048c9472065ce66
Amir Ahmed <amir.ahmed.ukb@gmail.com>
2024-07-08 14:38:13 +0000

    updated views and urls

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from books.views import bookstore
 
 urlpatterns = [
+    path('books/', bookstore, name='books'),
     path('admin/', admin.site.urls),
 ]
