f974b370872f0f24e825f1cad07caa5567e103db
Amir Ahmed <amir.ahmed.ukb@gmail.com>
2024-07-10 09:42:08 +0000

    updated settings paths and views and login, signup templates

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -83,7 +83,11 @@ ROOT_URLCONF = 'bookstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'books', 'templates'),
+            os.path.join(BASE_DIR, 'books', 'templates', 'books'),
+            os.path.join(BASE_DIR, 'books', 'templates', 'account'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -152,7 +156,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 STATIC_ROOT = 'staticfiles/'
 
 # Default primary key field type
