0446cf8c33e0e4c9099881e5bf878c1aee3d9c14
Amir Ahmed <amir.ahmed.ukb@gmail.com>
2024-07-15 06:37:17 +0000

    updated html files to include login required and display messages at the top

--- books/views.py
+++ books/views.py
@@ -20,7 +20,7 @@ def test_view(request):
     return HttpResponse("Hello, this is a test view to check Heroku deployment!")
 
 
-
+@login_required_message
 def author_list(request):
     query = request.GET.get('q')
     if query:
@@ -30,6 +30,7 @@ def author_list(request):
     
     return render(request, 'books/author_list.html', {'authors': authors, 'query': query})
 
+@login_required_message
 def author_detail(request, pk):
     author = get_object_or_404(Author, pk=pk)
     return render(request, 'books/author_detail.html', {'author': author})
@@ -72,6 +73,7 @@ def author_delete(request, pk):
         return redirect('author_list')
     return render(request, 'books/author_confirm_delete.html', {'author': author})
 
+@login_required_message
 def book_list(request):
     query = request.GET.get('q')
     if query:
@@ -81,6 +83,7 @@ def book_list(request):
     
     return render(request, 'books/book_list.html', {'books': books, 'query': query})
 
+@login_required_message
 def book_detail(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, 'books/book_detail.html', {'book': book})
